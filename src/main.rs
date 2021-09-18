95791b7b08c06dac63b8854062b1230e0071d8f1
Apua <Apua.A.Aa@gmail.com>
2021-09-18 23:52:29 +0800

    Add args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
-use clap::{App, load_yaml};
+use clap::{load_yaml, App};
 
 fn main() {
     let yaml = load_yaml!("args.yaml");
     let app = App::from(yaml).version("3.0");
-    app.get_matches();
+    let matches = app.get_matches();
+
+    let vs: Vec<&str> = matches.values_of("words").unwrap().collect();
+    println!("words → {:?}", vs);
+
+    println!("is present show provider → {:?}", matches.is_present("show provider"));
+    println!("is present show url → {:?}", matches.is_present("show url"));
+
+    let vs: Vec<&str> = matches.values_of("dict").unwrap().collect();
+    println!("dicts → {:?}", vs);
+
+    println!("is present list dicts → {:?}", matches.is_present("list dicts"));
+    println!("occurrences of verbose → {:?}", matches.occurrences_of("verbose"));
+
+    // cargo run -- --list-dicts --dict yahoo --dict all -vvv moe moe moe
+    //
+    // words → ["moe", "moe", "moe"]
+    // is present show provider → false
+    // is present show url → false
+    // dicts → ["yahoo", "all"]
+    // is present list dicts → true
+    // occurrences of verbose → 3
 }
