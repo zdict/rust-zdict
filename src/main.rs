65e15c0f9648a9e83649b63dac623b06ec392c46
Apua <Apua.A.Aa@gmail.com>
2021-09-19 12:52:20 +0800

    Set subcommand

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,34 @@ fn main() {
     let app = App::from(yaml).version("3.0");
     let matches = app.get_matches();
 
-    let vs: Vec<&str> = matches.values_of("words").unwrap().collect();
-    println!("words → {:?}", vs);
+    if matches.subcommand_matches("dicts").is_some() {
+        println!("⇒ dicts");
+        // :!cargo run -- dicts
+        //
+        // ⇒ dicts
+    } else if let Some(words) = matches.values_of::<&str>("words"){
+        println!("words ⇒ {:?}", words.collect::<Vec<_>>());
 
-    println!("is present show provider → {:?}", matches.is_present("show provider"));
-    println!("is present show url → {:?}", matches.is_present("show url"));
+        println!("is present show provider → {:?}", matches.is_present("show provider"));
+        println!("is present show url → {:?}", matches.is_present("show url"));
 
-    let vs: Vec<&str> = matches.values_of("dict").unwrap().collect();
-    println!("dicts → {:?}", vs);
+        let vs: Vec<&str> = matches.values_of("dicts").unwrap().collect();
+        println!("dicts → {:?}", vs);
 
-    println!("is present list dicts → {:?}", matches.is_present("list dicts"));
-    println!("occurrences of verbose → {:?}", matches.occurrences_of("verbose"));
+        println!("occurrences of verbose → {:?}", matches.occurrences_of("verbose"));
 
-    // cargo run -- --list-dicts --dict yahoo --dict all -vvv moe moe moe
-    //
-    // words → ["moe", "moe", "moe"]
-    // is present show provider → false
-    // is present show url → false
-    // dicts → ["yahoo", "all"]
-    // is present list dicts → true
-    // occurrences of verbose → 3
+        println!("is present list dicts → {:?}", matches.is_present("list dicts"));
+        //:!cargo run -- moe --dict all --dict all -vvv
+        //   Compiling zd v0.1.0 (/Users/apua/thoughts/rust_learn/zd)
+        //    Finished dev [unoptimized + debuginfo] target(s) in 1.07s
+        //     Running `target/debug/zd moe --dict all --dict all -vvv`
+        //words ⇒ ["moe"]
+        //is present show provider → false
+        //is present show url → false
+        //dicts → ["all", "all"]
+        //occurrences of verbose → 3
+        //is present list dicts → false
+
+    }
+
 }
