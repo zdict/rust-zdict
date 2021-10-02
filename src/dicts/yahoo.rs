1d06c28c4a784673858fec04adf528ec282b8955
Apua <Apua.A.Aa@gmail.com>
2021-10-02 14:19:32 +0800

    Implement parsing verbose

--- src/dicts/yahoo.rs
+++ src/dicts/yahoo.rs
@@ -1,4 +1,6 @@
 use super::{Lookup, Display};
+use reqwest;
+use kuchiki::traits::*;
 
 pub struct Dict;
 impl Lookup for Dict {
@@ -8,18 +10,61 @@ impl Lookup for Dict {
     const TITLE: &'static str = "Yahoo Dictionary";
     const PROVIDER: &'static str = "yahoo";
     type Record = Record;
-    fn query(&self, _url: &str) -> Self::Record {
-        Record { summary: "summary", explain: "explain", verbose: "verbose" }
+    fn query(&self, url: &str) -> Self::Record {
+        let response = reqwest::blocking::get(url).expect("...");
+        let content = response.text().expect("...");
+
+        let summary = parse_summary(&content);
+        let explain = parse_explain(&content);
+        let verbose = parse_verbose(&content);
+        Record { summary, explain, verbose }
     }
 }
 
+fn parse_summary(_content: &str) {  }
+fn parse_explain(_content: &str) {  }
+fn parse_verbose(content: &str) -> Option<i32> {
+    let document = kuchiki::parse_html().one(content);
+    let synonyms = document.select_first("div.tab-content-synonyms").unwrap();
+
+    use html5ever::local_name;
+    //assert_eq!(local_name!("div"), node.name.local);
+
+    //fn t<T> (_:&T) -> &str { std::any::type_name::<T>() }
+
+    for elm in synonyms.as_node().children().elements() {
+        match elm.name.local {
+            local_name!("div") => {
+                if let Ok(span) = elm.as_node().select_first(".fw-xl") {
+                    println!("[TODO] title -> {}", span.text_contents());
+                } else if let Ok(span) = elm.as_node().select_first(".fw-500") {
+                    println!("[TODO] explain -> {}", span.text_contents());
+                }
+            },
+            local_name!("ul") => {
+                //println!("[DEBUG] ul -> {:?}", elm); println!();
+                //let vs: Vec<String> = elm.as_node().select("li > span").unwrap().map(
+                //    |span| span.text_contents()
+                //    ).collect();
+                //println!("[TODO] span collect -> {:?}", vs);
+                for span in elm.as_node().select("li > span").unwrap() {
+                    println!("[TODO] item -> {}", span.text_contents())
+                };
+            },
+            _ => (),
+        }
+    }
+
+    None
+}
+
 pub struct Record {
-    summary: &'static str,
-    explain: &'static str,
-    verbose: &'static str,
+    summary: (),
+    explain: (),
+    verbose: Option<i32>,
 }
 impl Display for Record {
     fn show(&self) {
-        println!("yahoo record → {}, {}, {}", self.summary, self.explain, self.verbose)
+        println!("yahoo record → {:?}, {:?}, {:?}", self.summary, self.explain, self.verbose)
     }
 }
