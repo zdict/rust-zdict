a1ff259dcf4d236728b15b9f0d53cd557ef682a0
Apua <Apua.A.Aa@gmail.com>
2021-10-04 20:36:02 +0800

    Implement parsing explain

--- src/dicts/yahoo.rs
+++ src/dicts/yahoo.rs
@@ -1,6 +1,9 @@
 use super::{Lookup, Display};
 use reqwest;
 use kuchiki::traits::*;
+use kuchiki::{NodeRef, NodeDataRef, ElementData};
+use html5ever::local_name;
+use serde_json::{json, Value};
 
 pub struct Dict;
 impl Lookup for Dict {
@@ -13,55 +16,103 @@ impl Lookup for Dict {
     fn query(&self, url: &str) -> Self::Record {
         let response = reqwest::blocking::get(url).expect("...");
         let content = response.text().expect("...");
+        let document = kuchiki::parse_html().one(content);
 
-        let summary = parse_summary(&content);
-        let explain = parse_explain(&content);
-        let verbose = parse_verbose(&content);
-        Record { summary, explain, verbose }
+        Record {
+            summary: parse_summary(&document),
+            explain: parse_explain(&document),
+            verbose: parse_verbose(&document),
+        }
     }
 }
 
-fn parse_summary(_content: &str) {  }
-fn parse_explain(_content: &str) {  }
-fn parse_verbose(content: &str) -> Option<i32> {
-    let document = kuchiki::parse_html().one(content);
+fn parse_summary(_content: &NodeRef) -> Value {
+    json!(null)
+}
+
+macro_rules! next_text { ($nodes:expr) => ($nodes.next().unwrap().text_contents()) }
+fn parse_explain(document: &NodeRef) -> Value {
+    let explanation = document.select_first("div.tab-content-explanation").unwrap();
+    //println!("[DEBUG] explanation -> {:?}", explain);
+
+    let parse_item = |elm: NodeDataRef<ElementData>| {
+        let mut span_nodes = elm.as_node().select("span").unwrap();
+        let text = format!("{} {}", next_text!(span_nodes), next_text!(span_nodes));
+        let sentence: Value = span_nodes.map(|span| {
+            let mut piece: Vec<_> = span.as_node().children().map(|node|
+                if node.as_element().is_some() {
+                    json!(["b", node.text_contents()])
+                } else {
+                    json!(node.text_contents())
+                }
+            ).collect();
+
+            if let Some(Value::String(s)) = piece.pop() {
+                let (hd,tl) = s.rsplit_once(' ').unwrap();
+                piece.push(json!(hd));
+                piece.push(json!("\n"));
+                piece.push(json!(tl));
+                piece.push(json!("\n"));
+            }
+
+            piece
+        }).flatten().collect();
+
+        //println!("[DEBUG] sentence -> {:?}", sentence);
+        json!({"type": "item", "text": text, "sentence": sentence})
+    };
+
+    let elements = explanation.as_node().children().elements();
+    let explain: Value = elements.map(|elm| {
+        match elm.attributes.borrow().get("class").unwrap() {
+            cls_attr if cls_attr.contains("compTitle") => {
+                //println!("[DEBUG] cls attr => {:?}", cls_attr);
+                let j = json!({"type": "PoS", "text": elm.text_contents()});
+                //println!("[DEBUG] json => {:?}", j);
+                Some(j)
+            },
+            cls_attr if cls_attr.contains("compTextList") => {
+                //println!("[DEBUG] cls attr => {:?}", cls_attr);
+                let ul: Value = elm.as_node().select("li").unwrap().map(parse_item).collect();
+                //println!("[DEBUG] json => {:?}", ul);
+                Some(ul)
+            },
+            _ => None,
+        }
+    }).flatten().collect();
+
+    explain
+}
+
+fn parse_verbose(document: &NodeRef) -> Value {
     let synonyms = document.select_first("div.tab-content-synonyms").unwrap();
-
-    use html5ever::local_name;
-    //assert_eq!(local_name!("div"), node.name.local);
-
-    //fn t<T> (_:&T) -> &str { std::any::type_name::<T>() }
-
-    for elm in synonyms.as_node().children().elements() {
+    let verbose = synonyms.as_node().children().elements().map(|elm| {
         match elm.name.local {
             local_name!("div") => {
                 if let Ok(span) = elm.as_node().select_first(".fw-xl") {
-                    println!("[TODO] title -> {}", span.text_contents());
+                    Some(json!(["title", span.text_contents()]))
                 } else if let Ok(span) = elm.as_node().select_first(".fw-500") {
-                    println!("[TODO] explain -> {}", span.text_contents());
+                    Some(json!(["explain", span.text_contents()]))
+                } else {
+                    None
                 }
             },
             local_name!("ul") => {
-                //println!("[DEBUG] ul -> {:?}", elm); println!();
-                //let vs: Vec<String> = elm.as_node().select("li > span").unwrap().map(
-                //    |span| span.text_contents()
-                //    ).collect();
-                //println!("[TODO] span collect -> {:?}", vs);
-                for span in elm.as_node().select("li > span").unwrap() {
-                    println!("[TODO] item -> {}", span.text_contents())
-                };
+                Some(elm.as_node().select("li > span").unwrap().map(
+                    |span| json!(["item", span.text_contents()])
+                    ).collect())
             },
-            _ => (),
+            _ => None,
         }
-    }
+    }).flatten().collect();
 
-    None
+    verbose
 }
 
 pub struct Record {
-    summary: (),
-    explain: (),
-    verbose: Option<i32>,
+    summary: Value,
+    explain: Value,
+    verbose: Value,
 }
 impl Display for Record {
     fn show(&self) {
