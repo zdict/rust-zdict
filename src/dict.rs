4cd4aef65dd22e37b2b9c818ba6ffd3f636e2ae9
Apua <Apua.A.Aa@gmail.com>
2021-10-14 13:00:09 +0800

    Encap by generic fn + trait + pub struct

--- src/dict.rs
+++ src/dict.rs
@@ -13,15 +13,20 @@ macro_rules! register_dicts {
             )+
         }
 
-        pub fn lookup(word: &str, dict_name: &str, db_cache: &Cache, opts: &Opts) {
+        pub fn lookup_words(opts: Opts, db_cache: Cache) {
+            let (opts, db_cache) = (&opts, &db_cache);
+            let dict_name = opts.dict.as_ref().map_or("yahoo", |n| n.as_str());
             match dict_name {
-                $(
-                stringify!($d) => $d::Dict::lookup(word, db_cache, opts),
-                )+
+              $(
+                stringify!($d) => {
+                    for word in opts.words.as_slice() {
+                        lookup::<$d::Dict>(word, db_cache, opts);
+                    }
+                },
+              )+
                 _ => unreachable!(),
             }
         }
-
     };
 } register_dicts! { urban }
 
@@ -66,62 +71,6 @@ trait Lookup {
     fn get_query_url(word: &str) -> String;
 
     fn query(url: &str) -> QueryResult<Self::Content>;
-
-    fn lookup(word: &str, db_cache: &Cache, opts: &Opts) {
-        let url = Self::get_query_url(word);
-        let info = Self::INFO;
-
-        if opts.show_provider {
-            println!("\x1b[34m[{}]\x1b[0m", info.name);
-        }
-        if opts.show_url {
-            println!("\x1b[34m({})\x1b[0m", url);
-        }
-
-        let mut done = false;
-        if !opts.disable_db_cache {
-            if let Some(cached) =  db_cache.query(word, info.name) {
-                match Self::Content::from_str(cached.as_str()) {
-                    Ok(content) => {
-                        content.show(opts.verbose);
-                        done = true;
-                    },
-                    Err(err) => {
-                        // TODO: hadle below as an error
-                        log::warn!("fail to parse cached string");
-                        log::debug!("cached: {}", cached);
-                        log::debug!("err: {:?}", err);
-                    },
-                }
-            } else {
-                log::info!("cache not found");
-            }
-        }
-
-        if !done {
-            match Self::query(url.as_str()) {
-                Ok(content) => {
-                    //db_cache.save(word, info.name, content.to_string().as_str());
-                    content.show(opts.verbose);
-                },
-                Err(err) => {
-                    println!("{}", match err {
-                        QueryError::NotFound => format!("\x1b[33m\"{}\" not found!\x1b[0m", word),
-                        _ => format!("\
-                            ================================================================================\n\
-                            Dictionary: {} ({})\n\
-                            Word: '{}'\n\
-                            ================================================================================\n\
-                            \n\
-                            Houston, we got a problem 😢\n\
-                            Please report the error message above to https://github.com/zdict/zdict/issues\
-                        ", info.title, info.name, word),
-                    });
-                    log::debug!("{:?}", err);
-                },
-            }
-        }
-    }
 }
 
 
@@ -131,4 +80,62 @@ trait InnerStruct {
     fn to_string(&self) -> SerdeResult<String> where Self: Sized;
 
     fn show(&self, verbose: u8);
+}
+
+
+fn lookup<Dict: Lookup>(word: &str, db_cache: &Cache, opts: &Opts) {
+    let url = Dict::get_query_url(word);
+    let info = Dict::INFO;
+
+    if opts.show_provider {
+        println!("\x1b[34m[{}]\x1b[0m", info.name);
+    }
+    if opts.show_url {
+        println!("\x1b[34m({})\x1b[0m", url);
+    }
+
+    let mut done = false;
+    if !opts.disable_db_cache {
+        if let Some(cached) =  db_cache.query(word, info.name) {
+            match Dict::Content::from_str(cached.as_str()) {
+                Ok(content) => {
+                    content.show(opts.verbose);
+                    done = true;
+                },
+                Err(err) => {
+                    // TODO: handle error and print/log out
+                    log::warn!("fail to parse cached string");
+                    log::debug!("cached: {}", cached);
+                    log::debug!("err: {:?}", err);
+                },
+            }
+        } else {
+            log::info!("cache not found");
+        }
+    }
+
+    if !done {
+        // TODO: handle error and print/log out
+        match Dict::query(url.as_str()) {
+            Ok(content) => {
+                //db_cache.save(word, info.name, content.to_string().as_str());
+                content.show(opts.verbose);
+            },
+            Err(err) => {
+                println!("{}", match err {
+                    QueryError::NotFound => format!("\x1b[33m\"{}\" not found!\x1b[0m", word),
+                    _ => format!("\
+                        ================================================================================\n\
+                        Dictionary: {} ({})\n\
+                        Word: '{}'\n\
+                        ================================================================================\n\
+                        \n\
+                        Houston, we got a problem 😢\n\
+                        Please report the error message above to https://github.com/zdict/zdict/issues\
+                    ", info.title, info.name, word),
+                });
+                log::debug!("{:?}", err);
+            },
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,7 @@ fn main() {
     if opts.subcmd.is_some() {
         dict::list_dicts();
     } else {
-        let words = opts.words;
-        let db_cache = &db::Cache::new(opts.disable_db_cache);
-        let dict_name = opts.dict.as_ref().map_or("yahoo", |n| n.as_str());
-        let opts = &cli::Opts {
-            words: Default::default(),
-            dict: Default::default(),
-            .. opts
-        };
-        for word in words.as_slice() {
-            log::info!("lookup word {:?} by dict named {:?}", word, dict_name);
-            log::debug!("options: {:?}", opts);
-            dict::lookup(word, dict_name, db_cache, opts);
-        }
+        let db_cache = db::Cache::new(opts.disable_db_cache);
+        dict::lookup_words(opts, db_cache);
     }
 }
